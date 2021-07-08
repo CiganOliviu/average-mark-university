a814e973deca67253a0130a8b02434fba09373de
Cigan <ciganoliviudavid@gmail.com>
2021-07-08 23:15:43 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def calculate_class_mark(number_of_credits, mark):
+
+    return mark * number_of_credits
+
+
+def calculate_average_mark():
+    pass
+
+list_of_credits = []
+list_of_marks = []
+
+if __name__ == '__main__':
+    pass
+
+
+
