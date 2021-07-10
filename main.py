3eaeb469c08b772934f87956c4a533c403900440
Cigan <ciganoliviudavid@gmail.com>
2021-07-10 23:05:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def calculate_average_mark(credits_list, marks_list):
     return marks_sum / sum(list_of_credits)
 
 
-list_of_credits = [5, 5, 5, 3]
-list_of_marks = [10, 9, 7, 5]
+list_of_credits = [5, 5, 5, 5, 3, 3, 2, 2]
+list_of_marks = [7, 7, 8, 10, 8, 9, 9, 10]
 
 if __name__ == '__main__':
     print(calculate_average_mark(list_of_credits, list_of_marks))
